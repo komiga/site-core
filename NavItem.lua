2947bd7e1457704c2bb6df3ff948f1e9e15a78b2
Coranna Howard <me@komiga.com>
2016-02-18 01:47:03 -0500

    NavItem: don't escape text.

--- NavItem.lua
+++ NavItem.lua
@@ -7,9 +7,9 @@ U.class(M)
 
 local tpl = P.Template(nil, [[
 {% if not C.url then %}
-<li><span class="nav-item bold{% if C.class then %} {{ C.class }}{% end %}">{% if C.text then %}{{ C.text }}{% else %}:{% end %}</span></li>
+<li><span class="nav-item bold{% if C.class then %} {{ C.class }}{% end %}">{% if C.text then %}{! C.text !}{% else %}:{% end %}</span></li>
 {% else %}
-<li><a class="nav-item link{% if string.sub(C.url, 1, 1) ~= '#' then %} bold{% end %}{% if C.class then %} {{ C.class }}{% end %}"{% if C.title then %} title="{{ C.title }}"{% end %} href="{{ C.url }}">{% if C.icon then %}<img class="nav-item-icon{% if C.icon_class then %} {{ C.icon_class }}{% end %}" src="/images/icons/{{ C.icon }}" alt="{{ C.title }}"/>{% end %}{% if C.text then %}{{ C.text }}{% end %}</a></li>
+<li><a class="nav-item link{% if string.sub(C.url, 1, 1) ~= '#' then %} bold{% end %}{% if C.class then %} {{ C.class }}{% end %}"{% if C.title then %} title="{{ C.title }}"{% end %} href="{{ C.url }}">{% if C.icon then %}<img class="nav-item-icon{% if C.icon_class then %} {{ C.icon_class }}{% end %}" src="/images/icons/{{ C.icon }}" alt="{{ C.title }}"/>{% end %}{% if C.text then %}{! C.text !}{% end %}</a></li>
 {% end %}]])
 
 function M:__init(text, title, url, icon, icon_class, class)
