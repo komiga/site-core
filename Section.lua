c39309f6efeacba489113fc7e7071204590da98c
Coranna Howard <me@komiga.com>
2016-06-01 17:14:07 -0700

    Section: added pre- and post-text (i.e., non-url part) parameters.

--- Section.lua
+++ Section.lua
@@ -10,7 +10,7 @@ M.tpl_url = [[<a target="_blank" href="%s">%s</a>]]
 M.tpl_ref = [[<span class="para-ref" id="%s"><a href="#%s"></a></span>]]
 M.tpl_content = [[<h%d>%s%s</h%d>]]
 
-function M.make(page, name, text, url, id, level, add_nav)
+function M.make(page, name, text, url, id, level, add_nav, pre_text, post_text)
 	U.type_assert(page, Page, true)
 	U.type_assert(name, "string")
 	U.type_assert(text, "string")
@@ -18,6 +18,8 @@ function M.make(page, name, text, url, id, level, add_nav)
 	U.type_assert_any(id, {"boolean", "string"}, true)
 	level = U.optional(U.type_assert(level, "number", true), 1)
 	U.type_assert(add_nav, "boolean", true)
+	pre_text = U.type_assert(pre_text, "string", true) or ""
+	post_text = U.type_assert(post_text, "string", true) or ""
 
 	if not U.is_type(id, "boolean") then
 		id = id or slugize(name)
@@ -25,6 +27,7 @@ function M.make(page, name, text, url, id, level, add_nav)
 	if url then
 		text = string.format(M.tpl_url, url, text)
 	end
+	text = pre_text .. text .. post_text
 	if add_nav then
 		table.insert(page.nav, NavItem(text, nil, "#" .. id))
 	end
